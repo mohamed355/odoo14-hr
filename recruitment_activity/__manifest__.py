4c278c682d37dc2b84c88c309bb7990a16c8b9bd
salehaboezz <salehmohamedrizk@gmail.com>
2022-06-26 09:22:15 +0200

    {Ractivity}

---
+++ recruitment_activity/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Recruitment Activities',
+    'author': 'EzzEdin Saleh',
+    'depends': ['base','hr_recruitment','mail'],
+    'data': [
+        'views/mail_activity.xml',
+    ],
+}
