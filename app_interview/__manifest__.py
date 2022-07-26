27c3c0643d28a4dee421355b020839e31c4aa8ad
salehaboezz <salehmohamedrizk@gmail.com>
2022-07-26 19:03:31 +0200

    {rapp}

--- app_interview/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Interview',
-    'author': 'EzzEdin Saleh',
-    'depends': ['base','hr_recruitment'],
-    'data': [
-        'views/hr_applicant.xml',
-        'reports/interview_report.xml',
-             ],
-}
