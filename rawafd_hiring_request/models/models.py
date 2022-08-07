c25bf5b7b4aafbdd65ceb43050f3b33c27412a2d
salehaboezz <salehmohamedrizk@gmail.com>
2022-08-07 09:11:32 +0200

    {update6m133}

--- rawafd_hiring_request/models/models.py
+++ rawafd_hiring_request/models/models.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-import warnings
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore",category=DeprecationWarning)
-    import md5, sha
-
-
 from datetime import timedelta, date
 
 from odoo import models, fields, api, _
@@ -128,7 +121,6 @@ class HiringRequest(models.Model):
     _name = 'hiring.request'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    req_date = fields.Datetime(string="Request Date", required=False, )
     approve_date = fields.Datetime(string="Approve Date", required=False, )
     offered = fields.Integer(string="Offered", required=False)
     awaiting_review= fields.Integer(string="Awaiting Review", required=False)
@@ -225,7 +217,6 @@ class HiringRequest(models.Model):
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].next_by_code(
             'hiring.req.code')
-        vals['req_date'] = fields.Datetime.now()
         return super(HiringRequest, self).create(vals)
 
     def create_activities(self):
@@ -368,10 +359,7 @@ class stage(models.Model):
 
 class HrApplication(models.Model):
     _inherit = 'hr.applicant'
-    _rec_name = "partner_name"
 
-    name = fields.Char("Subject / Application Name", required=False, help="Email subject for applications sent via email")
-    approve_date = fields.Datetime(string="Approve Date", required=False, )
     hiring_ids = fields.Many2many(comodel_name="hiring.request",relation="asd", column1="df", column2="das", string="Hiring", )
     rej_boolean = fields.Boolean("rejected or no feedback")
 
@@ -417,16 +405,9 @@ class AssignApplications(models.Model):
             else:
                 hiring.update({'application_ids': [(4, application.id)]})
                 application.update({'hiring_ids': [(4, hiring.id)]})
-                application.approve_date = fields.Datetime.now()
 
         if apps_exist:
             raise ValidationError('This Applications %s Already Assigned ' % apps_exist)
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    approve_date = fields.Datetime(string="Approve Date", required=False, )
 
 
 class AssignUsers(models.Model):
@@ -437,6 +418,6 @@ class AssignUsers(models.Model):
     def assign_users(self):
         hiring = self.env['hiring.request'].browse(self.env.context.get('active_id'))
         hiring.approved=True
+        hiring.approve_date=fields.Datetime.now()
         for user in self.user_ids:
-            user.approve_date = fields.Datetime.now()
             hiring.update({'user_ids': [(4, user.id)]})
