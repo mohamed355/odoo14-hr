897462ae32f82faebacc701dbb5a39e18bbf029e
salehaboezz <salehmohamedrizk@gmail.com>
2022-08-07 10:04:41 +0200

    {updates78}

---
+++ history_update/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "History",
+    'depends': ['base', 'crm', 'recruitment_with_lead', 'mail','hr_recruitment','rawafd_hiring_request'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'security/security.xml',
+        'views/views.xml',
+        # 'views/templates.xml',
+        # 'data/ir_sequence.xml',
+    ],
+    # only loaded in demonstration mode
+
+}
---
+++ history_update/models/models.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta, date
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class HrApplication(models.Model):
+    _inherit = 'hr.applicant'
+    _rec_name = "partner_name"
+
+    name = fields.Char("Subject / Application Name", required=False, help="Email subject for applications sent via email")
+    approve_date = fields.Datetime(string="Approve Date", required=False, )
+    hiring_ids = fields.Many2many(comodel_name="hiring.request",relation="asd", column1="df", column2="das", string="Hiring", )
+    rej_boolean = fields.Boolean("rejected or no feedback")
+
+    # @api.depends()
+    # def _compute_hiring_ids(self):
+    #     for x in self:
+    #         if x.rej_boolean == False:
+    #             x.hiring_ids = self.env['hiring.request'].search([('application_ids','in',x.id)]).ids
+    #         else:
+    #             hiring_ids = self.env['hiring.request'].search([('application_ids','in',x.id)])
+    #             print(hiring_ids)
+    #             for h in hiring_ids:
+    #                 h.update({'application_ids': [(3,x.id,False)]})
+    #             x.hiring_ids = None
+
+    @api.constrains('stage_id')
+    def _constrains_stage_id(self):
+        for x in self:
+            if x.stage_id.name in ["Rejected", "No Feedback"]:
+                # x.rej_boolean = True
+                x.stage_id = None
+                x.hiring_ids = None
+                hiring_ids = self.env['hiring.request'].search([('application_ids','in',x.id)])
+                print(hiring_ids)
+                for h in hiring_ids:
+                    h.update({'application_ids': [(3,x.id,False)]})
+
+
+class AssignApplications(models.Model):
+    _name = 'assign.application'
+
+    name = fields.Char(default="Assign Applications")
+    applications_ids = fields.Many2many(comodel_name="hr.applicant", relation="hrapplicant", column1="hr", column2="applicant", string="Applications", )
+
+    def assign_applications(self):
+        hiring = self.env['hiring.request'].browse(self.env.context.get('active_id'))
+        apps_exist = []
+        for application in self.applications_ids:
+            if application.id in hiring.application_ids.ids:
+                apps_exist.append(application.name)
+            if application.hiring_ids:
+                raise ValidationError('This Applications Already In Hiring Request ')
+            else:
+                hiring.update({'application_ids': [(4, application.id)]})
+                application.update({'hiring_ids': [(4, hiring.id)]})
+                application.approve_date = fields.Datetime.now()
+
+        if apps_exist:
+            raise ValidationError('This Applications %s Already Assigned ' % apps_exist)
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    approve_date = fields.Datetime(string="Approve Date", required=False, )
+
+
+class AssignUsers(models.Model):
+    _name = 'assign.users'
+
+    user_ids = fields.Many2many(comodel_name="res.users", relation="resuser", column1="resuser", column2="ss", string="Users", )
+
+    def assign_users(self):
+        hiring = self.env['hiring.request'].browse(self.env.context.get('active_id'))
+        hiring.approved=True
+        for user in self.user_ids:
+            user.approve_date = fields.Datetime.now()
+            hiring.update({'user_ids': [(4, user.id)]})
