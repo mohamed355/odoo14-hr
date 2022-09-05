647dc1053f99cd1235eacfc19fe1ee81d6bec151
salehaboezz <salehmohamedrizk@gmail.com>
2022-09-05 12:58:20 +0200

    {5-9-update}

--- offer_template/__manifest__.py
+++ offer_template/__manifest__.py
@@ -3,7 +3,10 @@
     'author': 'EzzEdin Saleh',
     'depends': ['base', 'hr_recruitment', 'recruitment_with_lead', 'job_offer', 'mail', 'history_update'],
     'data': [
-        'views/hr_applicant.xml',
+        'reports/egy_report.xml',
+        'reports/ksa_report.xml',
+        'reports/saudi_report.xml',
+        # 'views/hr_applicant.xml',
         'data/email_template.xml',
     ],
 }
--- offer_template/models/hr_applicant.py
+++ offer_template/models/hr_applicant.py
@@ -17,3 +17,25 @@ class HrApplicant(models.Model):
             self.env['mail.template'].browse(template_sa.id).send_mail(self.id, force_send=True)
 
 
+class MailComposeMessage(models.TransientModel):
+    _inherit = 'mail.compose.message'
+
+    def default_template_id(self):
+        default = None
+        print(self.model)
+        if self.model == 'hr.applicant':
+            template_ksa = self.env.ref('offer_template.application_ksa_offer_template_ksa')
+            template_egy = self.env.ref('offer_template.application_ksa_offer_template_egy')
+            template_sa = self.env.ref('offer_template.application_ksa_offer_template_saudi_job')
+            app =self.env['hr.applicant'].browse(self.res_id)
+            if app.template == 'ksa':
+                default = template_ksa.id
+            if app.template == 'egy':
+                default = template_egy.id
+            if app.template == 'saudi':
+                default = template_sa.id
+            print(default)
+        return default
+    template_id = fields.Many2one(
+        'mail.template', 'Use template', index=True,
+        domain="[('model', '=', model)]",default = default_template_id)
