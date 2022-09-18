9d59f66ca9e587f4fb013f149f551cd0a9075a12
salehaboezz <salehmohamedrizk@gmail.com>
2022-09-18 16:53:46 +0200

    {18.2.2-9-update}

--- create_activity_from_hiring/models/hiring_request.py
+++ create_activity_from_hiring/models/hiring_request.py
@@ -10,17 +10,17 @@ class AssignUsers(models.TransientModel):
         hiring.approved = True
         hiring.acc_date = fields.Date.today()
         for user in self.user_ids:
+            hiring.update({'user_ids_real': [(4, user.id)]})
             self.env['user.history'].create({
                 'user_id': user.id,
                 'start_date': fields.Datetime.today(),
                 'hiring_id': hiring.id,
             })
             self.env['mail.activity'].create({
-                'activity_type_id': self.env['mail.activity.type'].search([('name','=','Sourcing')],limit=1).id,
+                'activity_type_id': self.env['mail.activity.type'].search([('name', '=', 'Sourcing')], limit=1).id,
                 'user_id': user.id,
                 'res_id': hiring.id,
                 'date_deadline': fields.Date.today() + timedelta(days=2),
                 'res_model_id': self.env['ir.model'].sudo().search(
                     [('model', '=', 'hiring.request')], limit=1).id,
             })
-            hiring.update({'user_ids_real': [(4, user.id)]})
