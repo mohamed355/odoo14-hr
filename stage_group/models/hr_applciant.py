18ff7070d5cef0e26ea243222c85dd2e90e3473c
salehaboezz <salehmohamedrizk@gmail.com>
2022-09-25 11:36:24 +0200

    {25.1.2-9-update}

--- stage_group/models/hr_applciant.py
+++ stage_group/models/hr_applciant.py
@@ -8,12 +8,13 @@ class HrApplicant(models.Model):
     @api.constrains('stage_id')
     def _constrains_stage_id_a(self):
         for x in self:
-            print(self.env.user.id, 'in', x.stage_id.user_ids.ids)
-            if self.env.user.id in x.stage_id.user_ids.ids:
-                print("Access")
-            else:
-                raise ValidationError("You Need To Access To Move In This Stage")
             print("adsd", x.stage_id.name)
+            print(self.env.user.id, 'in', x.stage_id.user_ids.ids)
+            if x.stage_id.name != False:
+                if self.env.user.id in x.stage_id.user_ids.ids:
+                    print("Access")
+                else:
+                    raise ValidationError("You Need To Access To Move In This Stage")
             if x.stage_id.name in ["Hold", "Completed"]:
                 x.stage_id = None
                 x.hiring_ids = None
