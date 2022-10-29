b522d68aa115ea0bf256b7102dc7156062b0ecae
salehaboezz <salehmohamedrizk@gmail.com>
2022-10-29 15:43:49 +0200

    {hiringreport}

---
+++ customer_hiring_report/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hiring Customers Lines",
+    'depends': ['rawafd_hiring_request'],
+    'author': 'Ezzedin Saleh',
+    'data': [
+        'views/customer_report.xml',
+    ],
+}
