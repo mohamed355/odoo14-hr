6cd75868d936a0cd477bfff6e4f87675d6b6be1e
salehaboezz <salehmohamedrizk@gmail.com>
2022-11-27 10:47:32 +0200

    {task}

--- app_interview/__manifest__.py
+++ app_interview/__manifest__.py
@@ -1,9 +1,11 @@
 {
     'name': 'Interview',
     'author': 'EzzEdin Saleh',
-    'depends': ['base','hr_recruitment','recruitment_with_lead'],
+    'depends': ['base', 'hr_recruitment', 'recruitment_with_lead'],
     'data': [
         'views/hr_applicant.xml',
         'reports/interview_report.xml',
-             ],
+        'reports/report.xml',
+        'reports/report_t.xml',
+    ],
 }
