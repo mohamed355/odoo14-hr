3fad66ec1d0954cdb034145bef8263803b59e2ad
salehaboezz <salehmohamedrizk@gmail.com>
2022-11-30 10:18:23 +0200

    {fields}

--- employee_inherit/models/hr_employee.py
+++ employee_inherit/models/hr_employee.py
@@ -6,8 +6,8 @@ class HrEmployee(models.Model):
 
     hr_leave_in_id = fields.Many2one(comodel_name="hr.leave.in", string="Live", required=False, )
     visa_type_id = fields.Many2one(comodel_name="visa.type", string="Visa Type", required=False, )
-    boundaries_id = fields.Many2one(comodel_name="boundaries", string="Boundaries N", required=False, )
-    accommodation_id = fields.Many2one(comodel_name="accommodation", string="Accommodation N", required=False, )
+    boundaries_id = fields.Integer(string="Boundaries N", required=False, )
+    accommodation_id = fields.Integer(string="Accommodation N", required=False, )
     acc_date = fields.Date(string="Accommodation date", required=False, )
 
 
